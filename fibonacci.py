bd8fbb1a2d7ce88698dea1a877419063e53234b1
PF <p_fire@126.com>
2014-11-07 08:37:36 -0500

    fibonacci.py

--- fibonacci.py
+++ fibonacci.py
@@ -1,7 +1,9 @@
 #! /usr/bin/env python
+import pf
+howMany = iinp("How many Fibonacci numbers should I create?")
 
 nums = [1, 1]
 print 1, 1, 
-while True:
+for i in range(2, howMany):
 	nums.append(nums[-2] + nums[-1])
 	print nums[-1], 
